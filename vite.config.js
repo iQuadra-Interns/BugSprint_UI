d5fed97105b441abc1c599c4abc3e6dfe0988502
iqHarishtalluru <127080564+iqHarishtalluru@users.noreply.github.com>
2024-06-20 15:16:15 +0530

    router plugin and folder structure

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // add this plugin to use routes
+  optimizeDeps: {
+    include: ['react-router-dom'],
+  },
 })

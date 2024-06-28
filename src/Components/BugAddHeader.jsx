3df85b28bf9bf75ce68a1af4801053170ff2151c
ZanyeNair <141592436+ZanyeNair@users.noreply.github.com>
2024-06-28 19:09:58 -0400

    components are not working

--- src/Components/BugAddHeader.jsx
+++ src/Components/BugAddHeader.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function BugAddHeader() {
   return (
@@ -20,6 +21,7 @@ function BugAddHeader() {
             </Row>
 
         </Container>
+        
         
       
     </div>

e4eb2e11d381deb7a49aab45023aaf3b5849f084
aavasar <abhinav.avasarala_intern@iquadra.com>
2024-07-03 21:02:55 -0400

    Added bug status color strips

--- src/components/Bug.jsx
+++ src/components/Bug.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Badge } from 'react-bootstrap';
+import { Badge} from 'react-bootstrap';
 
 export default function Bug({indbug}) {
 
@@ -18,11 +18,36 @@ export default function Bug({indbug}) {
         return <Badge bg={statusColors[status]}>{status}</Badge>;
       };
 
+    const getStatusColor = (status) => {
+        const statusColors = {
+          Open: '#dc3545',
+          Wip: '#ffc107',
+          Fixed: '#28a745',
+          Closed: '#28a745',
+          Deferred: '#17a2b8',
+          Rejected: '#6c757d',
+          Medium: '#ffc107',
+          Low: '#17a2b8',
+          High: '#dc3545'
+        };
+        return statusColors[status] || '#6c757d'; // Default color if status is not found
+      };
+
     return (
         <tr key={indbug.id}>
-            <td>
-              <div>{indbug.id}</div>
-              <div>{indbug.description}</div>
+            <td style={{ display: 'flex', alignItems: 'center' }}>
+              <div 
+                style={{
+                width: '4px',
+                height: '50px',
+                backgroundColor: getStatusColor(indbug.status),
+                marginRight: '10px'
+              }}
+              />
+              <div>
+                <div>{indbug.id}</div>
+                <div>{indbug.description}</div>
+              </div>
             </td>
             <td>{indbug.scenario}</td>
             <td>{getStatusBadge(indbug.status)}</td>

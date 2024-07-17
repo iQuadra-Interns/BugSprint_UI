d5130c3a0ee9ed346811673b4500df8482d58adf
ananya-iQuadra <160301216+ananya-iQuadra@users.noreply.github.com>
2024-07-17 00:02:31 -0400

    Css for the container and card is implemented

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -3,23 +3,27 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import {Row, Col, Container} from 'react-bootstrap'
 import Card from 'react-bootstrap/Card';
+import profile_pic from "../images/profile_pic.png";
+import './MyProfile.css'
 
 export default function MyProfile() {
     return (
-        <Container>
-            <Card>
+        <Container className="profileContainer">
+            <Card className="profileCard">
+                <img src={profile_pic} className="profilePicture" alt="Profile"/>
+                <h3 className="title">My Profile</h3>
                 <Row>
                     <Col>
-                        <p>Name:</p>
-                        <p>Role:</p>
-                        <p>Email:</p>
-                        <p>Contact:</p>
+                        <p className="description">Name:</p>
+                        <p className="description">Role:</p>
+                        <p className="description">Email:</p>
+                        <p className="description">Contact:</p>
                     </Col>
                     <Col>
-                        <p>Hari Krishna</p>
-                        <p>Admin</p>
-                        <p>hari.k@iquadra.com</p>
-                        <p>+91 9898989898</p>
+                        <p className="details">Hari Krishna</p>
+                        <p className="details">Admin</p>
+                        <p className="details">hari.k@iquadra.com</p>
+                        <p className="details">+91 9898989898</p>
                     </Col>
                 </Row>
             </Card>

66727c677cd0b0a021ff60e2e43d2a451bcbdfe0
ananya-iQuadra <160301216+ananya-iQuadra@users.noreply.github.com>
2024-07-29 22:49:16 -0400

    Adding routing and created new files so it can be merged to DEV

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import './App.css'
 // import NavigationBar from './components/NavigationBar';
-import SideBar from './components/Sidebar';
 import MyProfile from './components/MyProfile';
+import { useState } from 'react'
+import { createRoot } from "react-dom/client";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<MyProfile/>}/>
+    </Route>
+  )
+)
 
 function App() {
 
+  const [count, setCount] = useState(0)
+  
   return (
     <>
-      <Container fluid className="mainContainer">
-        <Row className="h-100 m-0">
-          <Col xs={2} className="p-0">
-            <SideBar />
-          </Col>
-          <Col xs={10}>
-            <MyProfile/>
-          </Col>
-        </Row>
-      </Container>
-      
+      <RouterProvider router={router}/>
     </>
   )
+
 }
 
 export default App

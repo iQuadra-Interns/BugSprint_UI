afdb85a2c30e5ff50e102545fefeb47a5fda200b
ZanyeNair <141592436+ZanyeNair@users.noreply.github.com>
2024-07-30 22:34:38 -0400

    Revert "Update package.json"
    This reverts commit 7b8e924fff214c0c19fce978cd539e0838181b51.

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Favicon from 'react-favicon'
+import {Helmet} from "react-helmet";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Helmet>
+      <title>BugSprint</title>
+    </Helmet>
+    <Favicon url='smallLogo.ico' />
     <App />
   </React.StrictMode>,
 )

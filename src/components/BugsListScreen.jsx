7a2066ba26652491d52881bb3c2ab5800a2cc3e5
ananya-iQuadra <160301216+ananya-iQuadra@users.noreply.github.com>
2024-08-07 20:33:01 -0400

    Update BugsListScreen.jsx

--- src/components/BugsListScreen.jsx
+++ src/components/BugsListScreen.jsx
@@ -7,8 +7,8 @@ function BugsListScreen() {
 
     return (
       <>
-        <Container fluid className="vh-100 p-0">
-        <Row className="h-100 m-0">
+        <Container fluid className="mainContainer">
+        <Row className="h-100">
           <Col xs={2} className="p-0">
             <SideBar />
           </Col>

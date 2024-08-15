1a4bdbc472fe90860323da5d747e5946c80c1e8b
ananya-iQuadra <160301216+ananya-iQuadra@users.noreply.github.com>
2024-08-14 22:54:58 -0400

    Tab resize attempts

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+/* const injectFavicon = () => {
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.type = 'image/svg+xml';
+  link.href = '/450_short_light.svg';
+  document.head.appendChild(link);
+}; */
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />

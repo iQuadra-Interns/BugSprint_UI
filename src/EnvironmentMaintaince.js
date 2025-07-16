f549432570dbde4f4e0c34f646b554792053feed
KeerthiPriya205 <keerthi.priya_intern@iquadra.com>
2025-07-16 14:50:30 +0530

    changes in env set up

--- src/EnvironmentMaintaince.js
+++ src/EnvironmentMaintaince.js
@@ -2,5 +2,5 @@ import allUrls from "./Baseurls";
 
   
   
-  export const env = import.meta.env.VITE_ENV
+  export const env = (import.meta.env.VITE_ENV || "DEV"); 
   export const baseurl = allUrls[env];
